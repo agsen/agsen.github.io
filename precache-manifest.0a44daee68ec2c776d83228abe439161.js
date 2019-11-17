3862252332d08bfaef2c594dc63f054162275317
Agung Setyo N <agungsn1994@gmail.com>
2019-11-18 02:43:31 +0700

    updated web v.1

---
+++ precache-manifest.0a44daee68ec2c776d83228abe439161.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f091e37f829334ca10cefe2a0eab85f8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b2c8473800393d45f5d3",
+    "url": "/static/css/main.d5cd5a1d.chunk.css"
+  },
+  {
+    "revision": "c0b395978c7d56000049",
+    "url": "/static/js/2.e0b950b3.chunk.js"
+  },
+  {
+    "revision": "b2c8473800393d45f5d3",
+    "url": "/static/js/main.2fe1acae.chunk.js"
+  },
+  {
+    "revision": "7565c46f720b3eaaabbd",
+    "url": "/static/js/runtime-main.e695090d.js"
+  },
+  {
+    "revision": "8446a5a823ba743fdbb005c1b44ac007",
+    "url": "/static/media/background.8446a5a8.jpg"
+  },
+  {
+    "revision": "75343b24cff351df82b7280181019a2a",
+    "url": "/static/media/cekongkir-min.75343b24.png"
+  },
+  {
+    "revision": "cf3f11fd5a8eb65dc3a0a7f90db3fe46",
+    "url": "/static/media/cekresi-min.cf3f11fd.png"
+  },
+  {
+    "revision": "0acd2469b770a49bc28175b906cfbdb0",
+    "url": "/static/media/demandAIimg1.0acd2469.png"
+  },
+  {
+    "revision": "ca553a33554577f3365c4aed608e8e91",
+    "url": "/static/media/demandAIimg2.ca553a33.png"
+  },
+  {
+    "revision": "a67452198af044fa4f33142681e54df7",
+    "url": "/static/media/foto.a6745219.jpg"
+  },
+  {
+    "revision": "d68e132bef944419aed1d1f53b13c40c",
+    "url": "/static/media/melisa-cover.d68e132b.jpg"
+  }
+]);

8d505c26d95a33d3150208a7c4543ada45308ff4
Agung Setyo N <agungsn1994@gmail.com>
2019-11-18 10:07:58 +0700

    update web v.2

---
+++ precache-manifest.e834f02a0056b66c28d912b194f0e1d3.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f909c77ba04ebf01e6202f022d72d75",
+    "url": "/index.html"
+  },
+  {
+    "revision": "84f3f957dc8894f9faa6",
+    "url": "/static/css/main.d5cd5a1d.chunk.css"
+  },
+  {
+    "revision": "7f929ac5ca6c5a83872c",
+    "url": "/static/js/2.029fe90b.chunk.js"
+  },
+  {
+    "revision": "84f3f957dc8894f9faa6",
+    "url": "/static/js/main.3509d872.chunk.js"
+  },
+  {
+    "revision": "7565c46f720b3eaaabbd",
+    "url": "/static/js/runtime-main.e695090d.js"
+  },
+  {
+    "revision": "8446a5a823ba743fdbb005c1b44ac007",
+    "url": "/static/media/background.8446a5a8.jpg"
+  },
+  {
+    "revision": "75343b24cff351df82b7280181019a2a",
+    "url": "/static/media/cekongkir-min.75343b24.png"
+  },
+  {
+    "revision": "cf3f11fd5a8eb65dc3a0a7f90db3fe46",
+    "url": "/static/media/cekresi-min.cf3f11fd.png"
+  },
+  {
+    "revision": "0acd2469b770a49bc28175b906cfbdb0",
+    "url": "/static/media/demandAIimg1.0acd2469.png"
+  },
+  {
+    "revision": "ca553a33554577f3365c4aed608e8e91",
+    "url": "/static/media/demandAIimg2.ca553a33.png"
+  },
+  {
+    "revision": "a67452198af044fa4f33142681e54df7",
+    "url": "/static/media/foto.a6745219.jpg"
+  },
+  {
+    "revision": "d68e132bef944419aed1d1f53b13c40c",
+    "url": "/static/media/melisa-cover.d68e132b.jpg"
+  },
+  {
+    "revision": "ca79cee24299fb779ffb2b47f9faed74",
+    "url": "/static/media/melisa2-min.ca79cee2.png"
+  },
+  {
+    "revision": "dc7c6efebe8f231281081fb1ac7b93f4",
+    "url": "/static/media/melisa3-min.dc7c6efe.png"
+  },
+  {
+    "revision": "f4064e21faba56eac0ca4b17e4b3ca0c",
+    "url": "/static/media/melisa4-min.f4064e21.png"
+  },
+  {
+    "revision": "6262e449160564619cd7f4e7601b4d98",
+    "url": "/static/media/melisa5-min.6262e449.png"
+  },
+  {
+    "revision": "2766b32aa2ec96a90a196c92c41d47f7",
+    "url": "/static/media/melisa6-min.2766b32a.png"
+  },
+  {
+    "revision": "4a7e0dc996041881755ad3f6459868a0",
+    "url": "/static/media/melisa7-min.4a7e0dc9.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0a44daee68ec2c776d83228abe439161.js"
+  "/precache-manifest.e834f02a0056b66c28d912b194f0e1d3.js"
 );
 
 self.addEventListener('message', (event) => {
